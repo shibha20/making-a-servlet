0be4e0ade408a83bf6f5bf028870d1f941b3e0cf
shilpabhandari <shilpabhandari@localhost>
2022-03-24 16:59:57 -0400

    adding a new servlet and redirecting to it

--- demoApp3/src/main/java/com/addServlet/AddServlet.java
+++ demoApp3/src/main/java/com/addServlet/AddServlet.java
@@ -2,22 +2,29 @@ package com.addServlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//every servlet class needs to extend httpservlet 
 public class AddServlet extends HttpServlet{
-	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	//every servlet method needs to be named service
+	//doPost to make sure it only works with a post request
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
 		
 		res.getWriter().println("Result is:" + k);
-
-
-
+		
+		//request dispatcher 
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 	
 
---
+++ demoApp3/src/main/java/com/addServlet/SqServlet.java
@@ -0,0 +1,17 @@
+package com.addServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out = res.getWriter();
+		out.println("Hello to sq");
+	}
+
+}
